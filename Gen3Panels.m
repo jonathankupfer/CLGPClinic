b9ea45b85d1aa8b7c685f3dc14f83b5b5f09e016
fjwalsh <fwalsh@hmc.edu>
2018-04-05 01:57:20 -0700

    Finished Gen3Panels.m
    It generates 3 panels! I also included in the description the libraries needed to run the function.

--- Gen3Panels.m
+++ Gen3Panels.m
@@ -7,60 +7,110 @@ function [W_panel,S_panel,E_panel] = Gen3Panels(type,date,samps)
 % samps is a scalar, and is the number of times you want to sample the
 % sunlight.
 
-%%%%%%%%%%%%%%%% unfinished!!!!! Last updated on Github 4/3 %%%%%%%%%%%%%%%%%%%%%%%%%
+% % Gen3Panels requires the following libraries: 
+% % SolarState.m
+% % UVector.m
+% % ProjectShadow.m
+% % GenCylinder.m
+% % You must also be running Matlab r2017b (available in Charlie). 
+
+%%%%%%% USER DEFINED PARAMETERS: %%%%%%%%%
 
 location = [34, -117]; % This is Claremont.
+SunsToWatts = 1000; % W/m^2
+shadeFactor = 0.25; % maybe this varies from idealPV to conventional? This
+                    % is the factor by which being in the stovepipe's 
+                    % shade decreases the sun's wattage. 
+
+%the cyl is the lower part of the stovepipe, the cone is the upper part.
+cylHeight=7; %inches
+cylRadius=5.5/2; %inches (i think this is overestimated)
+totalHeight=12; %inches
+coneHeight=3; %inches
+coneRadius=6.5; %inches
+pipeDistance=6.5; % inches (distance from bottom of panel to 
+                  % center of stovepipe
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Let's start by generating three panels. All 3 panels have the same
 %dimensions.
 %%%% These numbers are placeholders right now. They need to be verified.
 
-if type=='IdealPV'
-    panelDim=[2 4]; % width x height, in inches
-    cellDim=[5 6]; % width x height, in number of cells
-elseif type=='Conventional'
-    panelDim=[3 5]; % width x height, in inches]
-    cellDim=[50,60]; % width x height, in number of cells
+if strcmp(type,'IdealPV')==1
+    panelDim=[39 66]; % width x height, in inches
+    cellDim=[23 10]; % width x height, in number of cells
+elseif strcmp(type,'Conventional')==1
+    panelDim=[38.98 65.43]; % width x height, in inches]
+    cellDim=[6 10]; % width x height, in number of cells
+else 
+    return;
 end
 
 w=panelDim(1);
 h=panelDim(2);
-bounds = [0 w; 0 h];
-S_panel = polyshape([0 w w 0], [0 0 h h]); %S_panel is the only panel affected by the stovepipe.
 
-W_panelAngles=[56, 135];
-S_panelAngles=[56, 180]; %different coordinates 
-E_panelAngles=[56, 225];
 
-% E and W panels have constant insolation in unweathered conditions. 
-W_panel=GenerateIntensities(date, location, W_panelAngles, samps, cellDim);
-E_panel=GenerateIntensities(date, location, E_panelAngles, samps, cellDim);
+W_panelAngles=[56, -144];
+W_vector=UVector(W_panelAngles);
+S_panelAngles=[56, -90]; %different coordinates 
+S_vector=UVector(S_panelAngles);
+E_panelAngles=[56, -45];
+E_vector=UVector(E_panelAngles);
 
-% Now for the slightly less fun part, we have to cast some shade onto the
-% S panel. 
+%All the panels are the same size duh...
+W_panel = ones(cellDim(2), cellDim(1), samps);
+S_panel = ones(cellDim(2), cellDim(1), samps); 
+E_panel = ones(cellDim(2), cellDim(1), samps);
+
+%Now, time to generate today's sunrise and sunset times. 
+today=SolarState(date, 0, location);
+hours=linspace(today.t_sunrise, today.t_sunset, samps);
+
+% Initializing some variables for use on the south panel... 
 
 w_cells=linspace(0, w, cellDim(1));
 h_cells=linspace(0, h, cellDim(2));
-w_cells_1=reshape(repmat(w_cells,length(h_cells),1),[1,30]);
+w_cells_1=reshape(repmat(w_cells,length(h_cells),1),...
+    [1,cellDim(1)*cellDim(2)]);
 h_cells_1=repmat(h_cells,1,length(w_cells));
-%A vector with the coordinates of the center of each cell, in inches
 cellLocations=[w_cells_1;h_cells_1];
+strengthslist=ones(1,length(cellLocations));
 
-for n=1:((0:5:1080)/60+40)
-        sstate = SolarState(the_date, 9, location);
-    if sstate.hours < sstate.t_sunrise || sstate.hours > sstate.t_sunset
+%Now, a single unified for loop for all three panels. 
+
+for hour=linspace(1,samps, samps)
+    sst=SolarState(date,hours(hour),location);
+    W_suns=sst.suns*SunsToWatts*dot(sst.uSun,W_vector);
+    if W_suns<0
+        W_suns=0;
+    end
+    W_panel(:,:,hour)=W_panel(:,:,hour)*W_suns;
+    
+    S_suns=sst.suns*SunsToWatts*dot(sst.uSun,S_vector);
+    if S_suns<0
+        S_suns=0;
+    end
+    
+    E_suns=sst.suns*SunsToWatts*dot(sst.uSun,E_vector);
+    if E_suns<0
+        E_suns=0;
+    end
+    E_panel(:,:,hour)=E_panel(:,:,hour)*E_suns;
+   
+   
+    %shapes are situated in the center of the southern panel, which is
+    %assumed to have its origin at (0,0,0) for shadow projection.
+    cyl = Cylinder([0.5*w, -pipeDistance, 0], cylRadius, cylHeight,...
+        sst.uSun); 
+    cone = GenCylinder([0.5*w, -pipeDistance, totalHeight-coneHeight],...
+        [coneRadius cylRadius], coneHeight, sst.uSun);
+   
+    pts = ProjectShadow(cyl, sst.sun_angles, S_panelAngles, [0 0 0]);
+    cpts = ProjectShadow(cone, sst.sun_angles, S_panelAngles, [0 0 0]);
+    if isempty(pts) && isempty(cpts)
         continue;
     end
-    sunAngles = sstate.sun_angles;
-    uSolar = UVector(sunAngles);
-    cyl = Cylinder([1, -0.3, 0], 0.2, 1, uSolar);
-    cone = GenCylinder([1, -0.3, 1], [0.3 0.2], 0.5, uSolar);
-
-    pts = ProjectShadow(cyl, sunAngles, S_panelAngles, [0 0 0]);
-    cpts = ProjectShadow(cone, sunAngles, S_panelAngles, [0 0 0]);
-     if isempty(pts) && isempty(cpts)
-         continue;
-     end
     if isempty(pts)
         pshadow = polyshape(cpts);
     elseif isempty(cpts)
@@ -70,18 +120,16 @@ for n=1:((0:5:1080)/60+40)
             polyshape(pts(:,1), pts(:,2)));
     end
     
-    strengthslist=ones(1,length(cellLocations));
     for posn = 1:(length(cellLocations)-1)
-            if inpolygon(cellLocations(1,posn),cellLocations(2,posn),pshadow.Vertices(:,1),pshadow.Vertices(:,2))
-                strengthslist(posn)=0.25;
+            if inpolygon(cellLocations(1,posn),cellLocations(2,posn),...
+                    pshadow.Vertices(:,1),pshadow.Vertices(:,2))
+                strengthslist(posn)=shadeFactor;
+            else
+                strengthslist(posn)=1;
             end
     end
     
     shapematrix=flipud(reshape(strengthslist,cellDim(2),cellDim(1)));
-
-
-
-
+    S_panel(:,:,hour)=shapematrix*S_suns;
+    
 end
-%It's not 3D or normalized for angle yet.
-
